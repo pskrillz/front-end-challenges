8851a5387f51591cc5b64de1020e2468f3c48a13
pskrillz <payam.farah@live.com>
2020-04-24 18:33:15 -0700

    basic login page created with password validation js

--- lesson-5/js/main.js
+++ lesson-5/js/main.js
@@ -1,9 +1,15 @@
 
-/*
-* JAVASCRIPT FUNCTION
-* @parameter: This does ____
-*
-* Notes: Lorem ipsum justo vel
-* */
 
-console.log("This outputs into the console.");
+function validate(){
+    let username = document.getElementById('usernameinput').value;
+    let password = document.getElementById('passwordinput').value;
+    if (username !== 'username' && password !== 'password'){
+        alert('Username = username :::: Password = password')
+    } else if (username == 'username' && password == 'password') {
+        alert('logged in successfully')
+        window.location = "google.com"
+    } else {
+        alert('fail')
+    }
+    return false
+}
